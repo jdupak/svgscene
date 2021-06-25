e0affc7f3aec3a85a0c858ef6e1c1eb069503ec6
Jakub Dupak <dev@jakubdupak.com>
2021-06-25 12:15:22 +0200

    libshv backport: SVG SaxHandler tspan x,y coordinates handling improved

--- src/svgscene/svghandler.cpp
+++ src/svgscene/svghandler.cpp
@@ -1110,14 +1110,20 @@ bool SvgHandler::startElement() {
         } else if (el.name == QLatin1String("tspan")) {
             auto *item = new SimpleTextItem(el.styleAttributes);
             setElementMetadata(item, el);
-            qreal x = toDouble(el.xmlAttributes.value(QStringLiteral("x")));
-            qreal y = toDouble(el.xmlAttributes.value(QStringLiteral("y")));
-            // text->setPos(x, y);
             setStyle(item, el.styleAttributes);
             setTextStyle(item, el.styleAttributes);
             setTransform(item, el.xmlAttributes.value(QStringLiteral("transform")));
             QFontMetricsF fm(item->font());
             QTransform t;
+            qreal x = toDouble(el.xmlAttributes.value(QStringLiteral("x")));
+			qreal y = toDouble(el.xmlAttributes.value(QStringLiteral("y")));
+			if(x != 0 && y != 0 && m_topLevelItem) {
+				// https://www.w3.org/TR/SVG11/text.html#TSpanElement
+				QPointF p(x, y);
+				p = m_topLevelItem->mapFromScene(p);
+				//p = item->mapToParent(p);
+				t.translate(p.x(), p.y());
+			}
             t.translate(x, y - fm.ascent());
             item->setTransform(t, true);
             addItem(item);
